3447f85bda185aa670ae04237a2a8a2456ab92fb
Melanie Simpson <Melanie67@gmail.com>
2023-02-18 12:29:22 -0500

    request API info

--- app.js/src/App.js
+++ app.js/src/App.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import './App.css';
-import image from './movie-image.webp';
 import Title from './title';
 
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5181bc338fmsh2fe8c62891b6fa8p1f100fjsn0d5c5a73ef67',
-		'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-	}
-};
 
-fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>The Movie Source</h1> 
+        <input type="text" className="textbox" id="title" placeholder="Enter Movie Title"/><br></br>
+        
         <Title/>
-        <input type="text" class="textbox" placeholder="Enter Movie Title"/>&nbsp;
-        <button>Click for movie</button>&nbsp;
-        <img src={image} className="App-logo" alt="movie"/><br></br>
-        <span>Year</span>
-        <span>Rated</span>
-        <span>Genre</span>
-        <ul>
-          <li>Actors in here</li>
-        </ul>
-       
       </header>
     </div>
     
--- app.js/src/title.js
+++ app.js/src/title.js
@@ -1,15 +1,52 @@
 import React from 'react';
+import image from './movie-image.webp'; 
 
 class Title extends React.Component{
-    render(){
-      return (
+    constructor(){
+        super()
+    this.state= {
+        title:null,
+        year:null,
+        rated:null,
+        genre:null,
+        actors:[],
+    }
+    }
+    getNewMovie(){
+        let url = 'https://www.omdbapi.com/?i=tt3896198&apikey=ffb6cf87'
+        let movie = document.getElementById('title').value; 
+        document.getElementById('movie-title').innerHTML = movie;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                title:data.title,
+                year:data.year,
+                rated:data.rated,
+                genre:data.genre,
+                actors:data.actors,   
+        })
 
-        <h1>The Movie Source</h1>
-        
+        })
+    }
+    render(){ 
+      return ( 
+        <div>
+        <img src={image} className="App-logo" alt="movie"/>
+        <p className="cats" id="movie-title">Title: {this.state.title}</p>
+        <p className="cats">Year Released: {this.state.year}</p>
+        <p className="cats">Rating: {this.state.rated}</p>
+        <p className="cats">Genre: {this.state.genre}</p>
+        <ul className="cats">
+          <li>{this.state.actors}</li>
+        </ul>
+        <button type="button"  onClick={()=> this.getNewMovie()} className="button">Click for movie</button><br></br>
+    </div>
       )
     }
-  }
-
+}
+    
 
 
 
