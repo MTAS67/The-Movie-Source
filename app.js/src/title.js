7eba249e69713824b4a78cd9455e35d0d3c7c763
Melanie Simpson <Melanie67@gmail.com>
2023-02-21 19:13:27 -0500

    fix errors in code

--- app.js/src/title.js
+++ app.js/src/title.js
@@ -1,54 +1,72 @@
 import React from 'react';
-import image from './movie-image.webp'; 
+import image from './movie-image.webp';
 
-class Title extends React.Component{
-    constructor(){
-        super()
-    this.state= {
-        title:null,
-        year:null,
-        rated:null,
-        genre:null,
-        actors:[],
-    }
-    }
-    getNewMovie(){
-        let url = 'https://www.omdbapi.com/?i=tt3896198&apikey=ffb6cf87'
-        let movie = document.getElementById('title').value; 
-        document.getElementById('movie-title').innerHTML = movie;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            this.setState({
-                title:data.title,
-                year:data.year,
-                rated:data.rated,
-                genre:data.genre,
-                actors:data.actors,   
-        })
 
+
+class Title extends React.Component {
+  constructor() {
+    super() 
+    this.state = {
+      loadedMovie: false,
+      poster: null,
+      title: null,
+      year: null,
+      rated: null,
+      genre: null,
+      actors: [],
+      
+    }
+  }
+  getNewMovie() {
+    
+<img src={image} className="App-logo" alt="movie" /> 
+    let movie = document.getElementById('title').value;
+    
+    let url = `https://www.omdbapi.com/?t=${movie}&apikey=ffb6cf87`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loadedMovie: true,
+          poster: data.Poster,
+          title: data.Title,
+          year: data.Year,
+          rated: data.Rated,
+          genre: data.Genre,
+          actors: data.Actors,
         })
-    }
-    render(){ 
-      return ( 
-        <div>
-        <img src={image} className="App-logo" alt="movie"/>
-        <p className="cats" id="movie-title">Title: {this.state.title}</p>
-        <p className="cats">Year Released: {this.state.year}</p>
-        <p className="cats">Rating: {this.state.rated}</p>
-        <p className="cats">Genre: {this.state.genre}</p>
-        <ul className="cats">
-          <li>{this.state.actors}</li>
-        </ul>
-        <button type="button"  onClick={()=> this.getNewMovie()} className="button">Click for movie</button><br></br>
-    </div>
-      )
-    }
-}
+        console.log(data);
+
+      })
+
+  }
+  
+  render() {
+    
     
 
+    return (
+      <div>
+
+        {/*<img src={image} className="App-logo" alt="movie" />*/}
+        <img src={this.state.poster} alt="movie-poster"/>
+        <p className="cats" id="movie-title">Title: {this.state.title}</p>
+        <p className="cats">Year Released: {this.state.year}</p>
+        <p className="cats">Rated: {this.state.rated}</p>
+        <p className="cats">Genre: {this.state.genre}</p>
+        <ul className="cats">
+         
+          Actors: {this.state.actors}
+     
+        </ul>
+        <button type="button" onClick={() => this.getNewMovie()} className="button">Click for movie</button><br></br>
+      </div>
+    )
+  }
+}
 
 
 
-  export default Title; 
+
+
+export default Title; 
